7e498f97c81491cd77366459b33f109baf32bb62
cassiorachid <cassiorachid@gmail.com>
2020-10-28 21:48:12 -0300

    minor text correction

--- R/rms.dB.R
+++ R/rms.dB.R
@@ -11,7 +11,7 @@
 #' @param ... Further arguments passed to \code{\link[base]{mean}}.
 #'
 #' @details This function convert your dB data to linear values, compute the Root Mean Square (rms) and converts the result to dB again.
-#' @details This function was adaptet from \code{\link[seewave]{meandB}} and \code{\link[seewave]{rms}} functions from \code{\link[seewave]{seewave}} package. See their help for more details.
+#' @details This function was adapted from \code{\link[seewave]{meandB}} and \code{\link[seewave]{rms}} functions from \code{\link[seewave]{seewave}} package. See their help for more details.
 #'
 #' @references
 #'
