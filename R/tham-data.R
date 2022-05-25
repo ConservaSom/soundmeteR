549ebe4daf93379a4ec72f5bffe696ffd364ea07
Cássio Rachid Simões <cassiorachid@gmail.com>
2022-05-25 14:43:24 -0300

    creating example data tham

---
+++ R/tham-data.R
@@ -0,0 +1,12 @@
+#' \emph{Thamnophilus stictocephalus} song in a landscape
+#'
+#' @docType data
+#'
+#' @usage data(tham)
+#'
+#' @format An object of class \code{"Wave"}; see \link[tuneR]{readWave}.
+#'
+#' @source Recording by Ingrid Maria Denóbile Torres
+#'
+#' @details A landscape recording presenting \emph{Thamnophilus stictocephalus} song between 3.883035 and 7.044417 seconds. The "calib.value" for this record is 130.24.
+"tham"
