b47d9d454c292ba03c41134b0311476a8996ed5a
Cássio Rachid Simões <cassiorachid@gmail.com>
2022-06-10 19:42:01 -0300

    implementando pwrspec

---
+++ R/pwrspec.R
@@ -0,0 +1,84 @@
+#' Power Spectrum of a sound file
+#'
+#' @description  This functions computes a power spectrum from a sound file.
+#'
+#' @param file A wave file path in your computer or a class Wave object already loaded into R environment.
+#' @param from Numeric. The start time in seconds of the sample you want to analyze. Could also be relative to the end of the file (in negative values), see examples.
+#' @param to Numeric. The end time in seconds of the sample you want to analyze. Could also be relative to the end of the file (in negative values), see examples.
+#' @param bandpass A vector with length two with lower and upper limits of the band pass interval in Hz.
+#'
+#' @return This function returns a data.frame with Frequency (Hz) and Intensity (microPa) of the file.
+#'
+#' @references Power spectrum adapted from: Carcagno, S. 2013. Basic Sound Processing with R [Blog post]. Retrieved from http://samcarcagno.altervista.org/blog/basic-sound-processing-r/
+#' @references Miyara, F. 2017. Software-Based Acoustical Measurements. Springer. 429 pp. DOI: 10.1007/978-3-319-55871-4
+#'
+#' @examples
+#' data(tham)
+#' pwrspec(tham)
+#'
+#' #with from, to and bandpass
+#' pwrspec(tham, from=3.8, to=7.1, bandpass=c(900,2000))
+#'
+#' #from and to relative to duration (negatives values from the end of the soundfiles)
+#' pwrspec(tham, from=-3.49, to=-0.9, bandpass=c(900,2000))
+#'
+#'
+#' @export
+
+pwrspec <- function(file, from=0, to=Inf, bandpass=c(0,Inf)){
+
+  if(from < 0 & to < 0){ #ajustando para from e to relativo ao tamanho do arquivo
+
+    if(is.character(file)) dur=readWave(file, header = T) %>% #se for um arquivo para ler
+        data.frame() %>%
+        transmute(dur=samples/sample.rate) %>%
+        as.character()
+
+    if(class(file) == "Wave") dur=duration(file) #se for um arquivo já carregado no R
+
+    from=dur+from
+    to=dur+to
+  }
+
+
+  #Loading the sound file ####
+  if(class(file) == "Wave") som=extractWave(file, from = from, to=to, xunit="time", interact=F)
+  if(is.character(file)) som=readWave(file, from = from, to=to, units = "seconds")
+
+  #freee unused memore ####
+  rm(file)
+
+  #Trunc samples to duration with 3 decimal places if file bigger than 1s####
+  if(length(som)/som@samp.rate > 1){ #
+    if(!trunc((length(som)/som@samp.rate)*10^3)/10^3==length(som)/som@samp.rate){
+      som<-extractWave(som, to=(trunc((length(som)/som@samp.rate)*10^3)/10^3)*som@samp.rate, interact=F)
+      if(!trunc((length(som)/som@samp.rate)*10^3)/10^3==length(som)/som@samp.rate){message("Warning: It may take a little longer than usual to analyze this file (file duration with more than three decimal places)")}
+    }
+  }
+
+
+  s1 <- som@left/2^(som@bit-1) #scaled to the maximum possible (as result of '/2^(bitrate-1)')
+  n <- length(s1)
+  p <- fft(s1)
+  nUniquePts <- ceiling((n+1)/2)
+  p <- p[1:nUniquePts] #select just the first half since the second half is a mirror image of the first
+  p <- 2*(abs(p/n)) #changed here and next if/else in 2020.11.03 to match Miyara (2017) code routine in topic 8.6.6
+
+  if (n %% 2 > 0){  #Routine to remove Nyquist point. Odd nfft excludes Nyquist point
+    p[2:length(p)] <- p[2:length(p)]
+  } else {
+    p[2: (length(p) -1)] <- p[2: (length(p) -1)]
+  }
+
+  freqArray <- (0:(nUniquePts-1)) * (som@samp.rate / n) #create the frequency array
+
+  rm(som) #to free memory usage
+
+  espec<-data.frame(Freq.Hz=freqArray, Int.linear=p^2) #p^2 is part of Miyara (2017) code routine in topic 8.6.6
+
+  espec=espec %>%
+    filter(Freq.Hz >= bandpass[1] & Freq.Hz <= bandpass[2])
+
+  return(espec)
+
+}
