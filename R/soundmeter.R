5d65963761f0722b9ee77d211236b4e862fac1cf
Cássio Rachid Simões <cassiorachid@gmail.com>
2022-06-19 18:35:58 -0300

    rebuild soundmeter

--- R/soundmeter.R
+++ R/soundmeter.R
@@ -5,9 +5,7 @@
 #' @param CalibValue Anda de mãos dadas com calib position. Quando tem o position, ele é considerado o valor de referência, quando não tem o position, ele é considerado o valor de calibração.
 #' @param fw Character. Argument passed to \code{\link[seewave]{dBweight}} to indicate the frequency weighting curve to use on the anlysis. 'A', 'B', 'C', 'D', 'ITU', and 'none' are supported. See \code{\link[seewave]{dBweight}} for details. (By default: "none")
 #' @param tw Time weighting
-#' @param bandpass falta implementar...
-#' @param time.mess Logical. Activate or deactivate message of time to complete the function execution. (By default: \code{TRUE})
-#' @param stat.mess Logical. Activate or deactivate status message of the function execution. (By default: \code{TRUE})
+#' @param progressbar Logical. Activate or deactivate a progress bar with elapsed time and the last concluded file number. (By default: \code{TRUE})
 #' @param channel Only "left" or "right" acepted. By default "left"
 #' @param saveresults Logical. Set \code{TRUE} if you want to save a txt file with the results of the function execution. (By default: \code{FALSE})
 #' @param outname Character. If \code{saveresults} is \code{TRUE}, you can specify a name to appear on the txt file name after the default name. (By default: \code{NULL})
@@ -17,119 +15,142 @@
 #' @examples
 #' data("tham")
 #' soundmeter(tham, CalibValue = 130.24, tw="slow") #slow time window with calib value
-#' soundmeter(tham, CalibValue = NULL, tw="fast") #fast time window in dBFS
+#' soundmeter(tham, CalibValue = 130.24, tw="fast") #fast
+#'
+#' soundmeter(tham, CalibValue = 130.24, tw="fast", fw="A") #fast with frequency weight
+#'
+#' soundmeter(tham, CalibValue = NULL, tw="fast", ref=1) #fast time window in dBFS
 #'
 #' @export
 
-soundmeter <- function(files="wd", from=0, to=Inf, CalibPosition=NULL, CalibValue=0, ref=20, fw="none", bands="octaves", banpass=NULL, tw="fast", time.mess=T, stat.mess=T, channel="left", saveresults=F, outname=NULL){
+soundmeter <- function(files="wd", from=0, to=Inf, CalibPosition=NULL, CalibValue=NULL, ref=20, fw="none", bands="octaves", tw="fast", progressbar=T, channel="left", saveresults=F, outname=NULL){
 
-  start.time<-Sys.time()
-
-
-  if(class(files)=="Wave"){ #vendo o tipo de arquivo usado no imput e armazenando em um objeto ###
-    arquivos<-list(files)
-  }else if(is.list(files)){
-    arquivos<-files
+  if(class(files)=="Wave"){
+    files<-list(files)
   }else if(length(files)==1 && files=="wd") {
-    arquivos <- dir(pattern=".WAV", ignore.case=T)
-  }else if(is.character(files)) {
-    arquivos <- files
-  }else {
-    stop("Choose a valid file on the 'files' argument. Could be 'wd', a filename on your work directory, a character object containing filenames, or a list of wave files already loaded in the R environment", call. = F)
+    files <- dir(pattern=".WAV", ignore.case=T)
+  }else if(is.data.frame(files)){
+    files <- as.character(files)
   }
+
+  pb <- progress_bar$new(format = "[:bar]:percent [:elapsedfull || File :current/:total done]"
+                           , total = length(files)
+                           , complete = "="   # Completion bar character
+                           , incomplete = "-" # Incomplete bar character
+                           , current = ">"    # Current bar character
+                           , clear = FALSE    # If TRUE, clears the bar when finish
+                           #, width = 100     # Width of the progress bar
+  )
 
   #organizando a identificação de início e fim do trecho a analizar ####
-  if(is.list(arquivos)){
-    from=data.frame(1:length(arquivos), from)$from
-    to=data.frame(1:length(arquivos), to)$to
-    channel=data.frame(1:length(arquivos), channel)$channel
-  }else {
-    from=data.frame(arquivos, from)$from
-    to=data.frame(arquivos, to)$to
-    channel=data.frame(arquivos, channel)$channel
-  }
+  from=c(matrix(from, nrow=length(files)))
+  to=c(matrix(to, nrow=length(files)))
 
-  #início do loop maior ####
-  for(i in 1:length(arquivos)){
+  if(!(channel %in% c("left", "right")))stop("Only 'left' or 'right' acepted fo channel argument", call. = F)
+
+  if(!is.null(CalibValue) & !is.data.frame(CalibValue)){
+    CalibValue=matrix(CalibValue, nrow=length(files), ncol=1, byrow=T)
+  }else if(!is.null(CalibValue) & is.data.frame(CalibValue) && nrow(CalibValue) != length(files)) stop("When CalibValue is a data.frame, it must have the number of rows equal to files length.",call. = F)
+
+
+  #início do loop maior (por arquivo) ----
+  for(i in 1:length(files)){
+
+    if(channel == "right" && !som@stereo){
+      if(i == 1) stop(paste0("File ", files[[i]], " doesn't have a right channel."), call. = F)
+      warning(paste0("File ", files[[i]], " doesn't have a right channel."), call. = F)
+      next
+    }
+
+    if(!is.null(CalibPosition) && all(CalibPosition < 0)){ #ajustando calibposition
+
+      if(is.character(files[[i]])) dur=readWave(files[[i]], header = T) %>% #se for um arquivo para ler
+          data.frame() %>%
+          transmute(dur=samples/sample.rate) %>%
+          as.numeric()
+
+      if(class(files[[i]]) == "Wave") dur=duration(files[[i]]) #se for um arquivo já carregado no R
+
+      calib.ini=dur+CalibPosition[1]
+      calib.fin=dur+CalibPosition[2]
+    }else if(!is.null(CalibPosition)){
+
+      calib.ini=CalibPosition[1]
+      calib.fin=CalibPosition[2]
+
+    }
+
+    #calibrando ----
+    if(exists("calib.ini") && exists("calib.fin")){
+      if(class(files[[i]])=="Wave"){
+        som<-extractWave(files[[i]], from=calib.ini, to=calib.fin, xunit = "time", interact = F)
+      } else {
+        som<-readWave(files[[i]], from = calib.ini, to=calib.fin, units = "seconds")
+      }
+
+      CalibValue[i]=Tweighting(som, channel=channel, window=tw, bands="thirds", Leq.calib=CalibValue[i], weighting=fw, ref=ref)
+
+      rm(som)
+    }
+
 
     #Reading sound file ####
-    if(class(arquivos[[i]])=="Wave"){
-      som<-arquivos[[i]]
+    if(from[i]<0 && to[i]<0){ #ajustando from & to
+
+      if(is.character(files[[i]])) dur=readWave(files[[i]], header = T) %>% #se for um arquivo para ler
+          data.frame() %>%
+          transmute(dur=samples/sample.rate) %>%
+          as.numeric()
+
+      if(class(files[[i]]) == "Wave") dur=duration(files[[i]]) #se for um arquivo já carregado no R
+
+      from[i]=dur+from[i]
+      to[i]=dur+to[i]
+    }
+
+    if(class(files[[i]])=="Wave"){
+      som<-extractWave(files[[i]], from=from[i], to=to[i], xunit = "time", interact = F)
     } else {
-      som<-readWave(arquivos[[i]])
+      som<-readWave(files[[i]], from = from[i], to=to[i], units = "seconds")
     }
 
-    #Extraindo o canal ####
-    if(channel[i] == "right"){
-      som=mono(som, "right")
+    #Leq & medidas estatísticas
+    if(!is.null(CalibValue)){
+      matriz=Tweighting(som, channel=channel, window=tw, bands=bands, weighting=fw, ref=ref, Calib.value=CalibValue[i])
     } else{
-      som=mono(som, "left")
+      matriz=Tweighting(som, channel=channel, window=tw, bands=bands, weighting=fw, ref=ref)
     }
 
-    #Ajustando informações de CalibValue ####
-    if(!is.null(CalibValue) & length(CalibValue)>1){
+    rm(som)
 
-      if(i==1 & length(CalibValue)!=length(arquivos)) stop("When specifying multiples CalibValues, it and files must be the same length")
-
-      cal.val=CalibValue[i]
-
-    }else {cal.val=CalibValue}
-
-
-    #Análise sem calibração (em dBFS) ####
-    if(is.null(CalibValue)){
-
-      matriz=Tweighting(extractWave(som, from=from[i] , to=to[i], xunit = "time", interact=F), window=tw, bands=bands, weighting="none")
-
-    }else if(!is.null(CalibPosition) & !is.null(CalibValue)){#Organizando extraindo valor de calibração ####
-
-      #Verificando de o local da calibração veio num data frame ou é valor único para todos
-      if(is.data.frame(CalibPosition)){ #se for data. frame
-
-        if(i==1 & nrow(CalibPosition)!=length(arquivos) & ncol(CalibPosition)!=2) stop("When CalibPosition is a data frame, it must be the same length of files")
-        cal.pos = CalibPosition[i,]
-
-      }else if(is.numeric(CalibPosition) & length(CalibPosition) == 2){#se for valores únicos para todos os arquivos
-
-        cal.pos=CalibPosition
-
-      }else {stop("Something went wrong, please check your CalibPosition")}
-
-      #Verificando se o local da calibração é por tempo ou relativo à duração do arquivo
-      if(all(cal.pos < 0) & duration(som)>=cal.pos[2]-cal.pos[1]){
-
-        cal.pos = c(duration(som)+cal.pos[1], duration(som)+cal.pos[2])
-
-      }else if(all(!(cal.pos >= 0) | duration(som)<cal.pos[2]-cal.pos[1]) | any(is.na(cal.pos))){stop("Some values of your CalibPosition are wrong")}
-
-
-      #calibração propriamente dita ####
-      cal.val=Tweighting(extractWave(som, from=cal.pos[1] , to=cal.pos[2], xunit = "time", interact=F), window=tw, bands="thirds", Leq.calib=cal.val, weighting="none") #considerando curva Z p calibração
-
-    }
-
-    # if(exists("cal.val")){
-      #Usando CalibValue como valor de calibração diretamente ####
-      matriz=Tweighting(extractWave(som, from=from[i] , to=to[i], xunit = "time", interact=F), window=tw, bands=bands, weighting=fw, Calib.value = cal.val)
-    # }
-
-
-    #criando e armazenando valores na matriz de resultados ####
+    #criando e armazenando valores na matriz de resultados ----
     if(i == 1){
-      res = data.frame(matrix(data=NA,nrow=length(arquivos),ncol=8+ncol(matriz[-1])))
-      colnames(res) = c("File", "Lmin", "Lmax", "L90", "L50", "L10", "Lpeak", "Leq", colnames(matriz[-1]))
+      res = data.frame(matrix(data=NA,nrow=length(files),ncol=7+ncol(matriz[-1])))
+      colnames(res) = c("File", "min", "max", "90", "50", "10", "eq", colnames(matriz[-1]))
+
+      if(fw == "none"){
+        colnames(res)[2:7]=paste0("L", colnames(res)[2:7])
+      }else{
+        colnames(res)[2:7]=paste0("L", fw, colnames(res)[2:7])
+      }
+
+      if(is.list(files) & !is.null(names(files))){
+        res$File = names(files)
+      }else if(!is.list(files)){
+        res$File=files
+      }else {
+        res$File=1:length(files)
+      }
     }
 
-    res$File[i]=ifelse(class(arquivos[[i]]) == "Wave", i, arquivos[[i]]) #Nome ou número do arquivo
-    res$Lmin[i]=min(matriz$Leq) #Lmin
-    res$Lmax[i]=max(matriz$Leq) #Lmax
 
-    res[i,c("L90","L50","L10")] = LineartodB(quantile(dBtoLinear(matriz$Leq, factor = "SPL", ref = ref), probs=c(0.1, 0.5, 0.9)), factor = "SPL", ref = ref) #L90,L50 e L10
+    #AJUSTAS REFERENCIAS ABAIXO!
+    res[i,2]=min(matriz$Leq) #Lmin
+    res[i,3]=max(matriz$Leq) #Lmax
 
-    res$Lpeak[i]=LineartodB(max(abs(extractWave(som, from=from[i] , to=to[i], xunit = "time", interact=F)@left/2^(som@bit -1))), factor="SPL", ref=ref) #Lpeak
-    if(!is.null(cal.val)) res$Lpeak[i] = res$Lpeak[i]+cal.val
+    res[i,4:6] = LineartodB(quantile(dBtoLinear(matriz$Leq, factor = "SPL", ref = ref), probs=c(0.1, 0.5, 0.9)), factor = "SPL", ref = ref) #L90,L50 e L10
 
-    res[i,8:ncol(res)]=apply(matriz, 2, meandB, level="SPL") #Leq e bandas
+    res[i,7:ncol(res)]=apply(matriz, 2, meandB, level="SPL") #Leq e bandas
 
     res[i,-1]=round(res[i,-1],2) #arredondando valores para duas casas decimais
 
@@ -143,13 +164,13 @@ soundmeter <- function(files="wd", from=0, to=Inf, CalibPosition=NULL, CalibValu
                   ,row.names = F, col.names = T,sep = "\t", quote=F)
     }
 
-    if(stat.mess){cat(c("File", i, "of", length(arquivos), "done."), sep=" ", fill = T)} #stat.mess ----
+    if(progressbar) pb$tick()
 
   } #final do loop maior ####
 
-  if(time.mess){message(cat(c("The code has run in ", format(Sys.time()-start.time), "."), sep = ""))}
-
   return(res)
+
+  pb$terminate()
 
 }
 
