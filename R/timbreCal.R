41b6d007c7f260ff5925ecef40b6d3a053ed1828
Cássio Rachid Simões <cassiorachid@gmail.com>
2023-11-10 18:28:18 -0300

    add from, to, calib position and value

--- R/timbreCal.R
+++ R/timbreCal.R
@@ -8,8 +8,8 @@
 #' @param channel Argument passed to \link[tuneR]{mono} function from \link[tuneR]{tuneR} to extract the desired channel.
 #' @param from Numeric. The start time in seconds of the sample you want to analyze. Could also be relative to the end of the file (in negative values), see examples.
 #' @param to Numeric. The end time in seconds of the sample you want to analyze. Could also be relative to the end of the file (in negative values), see examples.
-#' @param SignalDur Numerical. Specify the reference signal duration (in seconds) on the beggining of the audiofile. (By default: \code{NULL})
-#' @param RefValue Numerical. Specify the reference signal sound pressure level (in deciBells SPL) on the beggining of the audiofile. (By default: \code{NULL})
+#' @param CalibPosition anda de mãos dadas com calib value. Pode ser negativo, positivo ou data.frame com essas combinações
+#' @param CalibValue Anda de mãos dadas com calib position. Quando tem o position, ele é considerado o valor de referência, quando não tem o position, ele é considerado o valor de calibração.
 #' @param ref Numerical. The reference value for dB conversion. For sound in water, the common is 1 microPa, and for sound on air 20 microPa. (By default 20)
 #' @param weighting Character. Indicate the weighting curve to use on the anlysis. A, B, C and none are supported. (By default: "none")
 #' @param bands Character. Choose the type of frequency band of the output. "octaves" to octaves bands intervals or "thirds" to one-third octaves bands intervals. (by deafault: "thirds")
@@ -29,7 +29,7 @@
 #Pensar ao invés de usar um trecho da gravação para calibrar usar um arquivo externo.
 
 
-timbreCal<- function(files="wd", channel="left", from=0, to=Inf,
+timbreCal <- function(files="wd", channel="left", from=0, to=Inf,
                      CalibPosition=NULL, CalibValue=NULL, ref=20,
                      weighting="none", bands="thirds", saveresults=F,
                      outname=NULL, progressbar=T){
@@ -54,39 +54,76 @@ timbreCal<- function(files="wd", channel="left", from=0, to=Inf,
     )
   }
 
-  for(i in 1:length(files)){ #Loop que analisara os files####
+  #organizando a identificação de início e fim do trecho a analizar ----
+  from=c(matrix(from, nrow=length(files)))
+  to=c(matrix(to, nrow=length(files)))
 
-    if(class(files[[i]])=="Wave"){ #isolando o sinal de refer?ncia do som ####
-      som<-mono(extractWave(files[[i]], from=2, to=SignalDur+2, xunit="time",
-                            interact=F), channel)
-    } else {
-      som<-mono(readWave(files[[i]], from=2, to=SignalDur+2, units="seconds"),
-                channel)
+  if(!(channel %in% c("left", "right"))){
+    stop("Only 'left' or 'right' acepted fo channel argument", call. = F)
+  }
+
+  if(!is.null(CalibValue) & !is.data.frame(CalibValue)){
+    CalibValue=matrix(CalibValue, nrow=length(files), ncol=1, byrow=T)
+  }else if(!is.null(CalibValue) & is.data.frame(CalibValue) &&
+           nrow(CalibValue) != length(files)){
+    stop("When CalibValue is a data.frame, it must have the number of rows equal to files length.",call. = F)
+  }
+
+  #Loop que analisara os files ----
+  for(i in 1:length(files)){
+
+    if(!is.null(CalibPosition) && all(CalibPosition < 0)){ #ajustando calibposition
+
+      if(is.character(files[[i]])){ #se for um arquivo para ler
+        dur=readWave(files[[i]], header = T) %>%
+          data.frame() %>%
+          transmute(dur=samples/sample.rate) %>%
+          as.numeric()
+      }
+
+      if(class(files[[i]]) == "Wave"){ #se for um arquivo já carregado no R
+        dur=duration(files[[i]])
+      }
+
+      calib.ini=dur+CalibPosition[1]
+      calib.fin=dur+CalibPosition[2]
+
+    }else if(!is.null(CalibPosition)){
+
+      calib.ini=CalibPosition[1]
+      calib.fin=CalibPosition[2]
+
     }
 
-    calib.value<-timbre(files=som, channel = channel, Leq.calib=RefValue,
-                        ref=ref, Calib.value=NULL, saveresults=F, outname=NULL,
-                        weighting=weighting, progressbar=F) #Analisa o sinal de referencia e obtem o valor de calibracao ####
+    #calibrando ----
+    if(exists("calib.ini") && exists("calib.fin")){
+      if(class(files[[i]])=="Wave"){
+        som<-extractWave(files[[i]], from=calib.ini, to=calib.fin,
+                         xunit = "time", interact = F)
+      } else {
+        som<-readWave(files[[i]], from = calib.ini, to=calib.fin,
+                      units = "seconds")
+      }
 
-    calib.value<-calib.value[,2]
+      CalibValue[i]=timbre(som, channel=channel, Leq.calib=CalibValue[i],
+                           weighting=weighting, ref=ref, progressbar=F)$Calib.value
 
-    if(class(files[[i]])=="Wave"){ #isolando o som a ser analisado ####
-      som<-mono(extractWave(files[[i]], from=4+SignalDur, to=Inf,
-                            xunit="time", interact=F), channel)
-    } else {
-      som<-mono(readWave(files[[i]], from=4+SignalDur, units="seconds"  ),
-                channel)
+      rm(som)
     }
 
     if(i==1){#gerando a matriz de resultados ####
-      results<-timbre(files=som, channel = channel, Calib.value=calib.value,
-                      ref=ref, Leq.calib=NULL, saveresults=F, outname=NULL,
+      results<-timbre(files=files[[i]], from=from[i], to=to[i],
+                      channel=channel, Calib.value=CalibValue[i], ref=ref,
                       weighting=weighting, bands=bands, progressbar=F)
-    }else {results<-rbind(results,
-                          timbre(files=som, channel = channel, Calib.value=calib.value,
-                                 ref=ref, Leq.calib=NULL, saveresults=F, outname=NULL,
-                                 weighting=weighting, bands=bands, progressbar=F)
-    )}
+
+    }else {
+      results<-rbind(results,
+                     timbre(files=files[[i]], from=from[i], to=to[i],
+                            channel=channel, Calib.value=CalibValue[i],
+                            ref=ref, weighting=weighting, bands=bands,
+                            progressbar=F)
+      )
+    }
 
     if(is.list(files) && is.null(names(files))){ # colocando o nome dos files na primeira coluna####
       results[i,1]<-i
