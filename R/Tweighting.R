764ab84289f4e1d89d51b38723afd230ee7fac3a
Cássio Rachid simões <cassiorachid@gmail.com>
2024-11-30 06:26:11 -0300

    minor identation

--- R/Tweighting.R
+++ R/Tweighting.R
@@ -19,55 +19,56 @@
 #' @seealso \code{\link{leqbands}}, \code{\link{soundmeter}}
 #'
 #' @examples
-#' #creating an example sound file
-#' som=sine(1000, duration = 44500)
+#' # creating an example sound file
+#' som <- sine(1000, duration = 44500)
 #'
-#' #default options without calibration (results in dBFS)
+#' # default options without calibration (results in dBFS)
 #' Tweighting(som)
 #'
-#' #Simulation of a calib signal with a Leq of 94dB in the field ####
-#' #fast
-#' Tweighting(som, window = "fast", bands="octaves", Leq.calib=94)
-#' #slow
-#' Tweighting(som, window = "slow", bands="octaves", Leq.calib=94)
+#' # Simulation of a calib signal with a Leq of 94dB in the field ####
+#' # fast
+#' Tweighting(som, window = "fast", bands = "octaves", Leq.calib = 94)
+#' # slow
+#' Tweighting(som, window = "slow", bands = "octaves", Leq.calib = 94)
 #'
-#' #Using the result of the simulation above to calibrate the sound and output
-#' #fast
-#' Tweighting(som, window = "fast", bands="octaves", Calib.value=309.67)
-#' #slow
-#' Tweighting(som, window = "slow", bands="octaves", Calib.value=309.67)
+#' # Using the result of the simulation above to calibrate the sound and output
+#' # fast
+#' Tweighting(som, window = "fast", bands = "octaves", Calib.value = 309.67)
+#' # slow
+#' Tweighting(som, window = "slow", bands = "octaves", Calib.value = 309.67)
 #'
-#' #With tham data
+#' # With tham data
 #' data(tham)
-#' Tweighting(tham, window = "fast", bands="octaves", Calib.value=130.24) #fast
-#' Tweighting(tham, window = "slow", bands="octaves", Calib.value=130.24) #slow
+#' Tweighting(tham, window = "fast", bands = "octaves", Calib.value = 130.24) # fast
+#' Tweighting(tham, window = "slow", bands = "octaves", Calib.value = 130.24) # slow
 #'
 #' @export
 
 
-Tweighting <- function(file, window="fast", Leq.calib=NULL,...){
-
-  if(window == "fast"){
-    window = 0.125
-  }else if(window == "slow"){
-    window = 1
-  }else stop("Choose a valid window size in seconds ('fast' or 'slow')")
-
-  if(class(file) != 'Wave') stop("Only one Wave object accepted on this function")
-
-  res=sapply(1:trunc(duration(file)/window)
-             , FUN=function(x, file, samp){
-               return(leqbands(extractWave(file, from = round((x-1)*samp), to=round(x*samp)), progressbar=F, Leq.calib=NULL, ...)[,-1])
-             }
-             , file=file, samp=window*file@samp.rate)
-
-  if(!is.numeric(res)){
-    res=t(res)
-    res=as.data.frame(matrix(unlist(res), nrow = nrow(res), byrow = F, dimnames = list(NULL, colnames(res))), check.names=F) #convertendo em data.frame p facilitar manuseio
+Tweighting <- function(file, window = "fast", Leq.calib = NULL, ...) {
+  if (window == "fast") {
+    window <- 0.125
+  } else if (window == "slow") {
+    window <- 1
+  } else {
+    stop("Choose a valid window size in seconds ('fast' or 'slow')")
   }
 
-  if(!is.null(Leq.calib)) res=Leq.calib - seewave::meandB(res$Leq, level = "SPL") #Extraindo valor de calibração a partir do da média dos valores de RMS (exatamente o Leq)
+  if (class(file) != "Wave") stop("Only one Wave object accepted on this function")
+
+  res <- sapply(1:trunc(duration(file) / window),
+    FUN = function(x, file, samp) {
+      return(leqbands(extractWave(file, from = round((x - 1) * samp), to = round(x * samp)), progressbar = F, Leq.calib = NULL, ...)[, -1])
+    },
+    file = file, samp = window * file@samp.rate
+  )
+
+  if (!is.numeric(res)) {
+    res <- t(res)
+    res <- as.data.frame(matrix(unlist(res), nrow = nrow(res), byrow = F, dimnames = list(NULL, colnames(res))), check.names = F) # convertendo em data.frame p facilitar manuseio
+  }
+
+  if (!is.null(Leq.calib)) res <- Leq.calib - seewave::meandB(res$Leq, level = "SPL") # Extraindo valor de calibração a partir do da média dos valores de RMS (exatamente o Leq)
 
   return(res)
-
 }
