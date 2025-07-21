e986f14daf2104789e620425a37b2d7934c2b86e
Ingrid Maria Denóbile Torres <74189608+IMDTorres@users.noreply.github.com>
2025-07-21 17:32:31 -0300

    Update rms.dB.R
    revisao textual

--- R/rms.dB.R
+++ R/rms.dB.R
@@ -2,14 +2,14 @@
 #'
 #' @description Function to compute the root mean square (RMS) of values in decibels (dB).
 #'
-#' @param x Numerical. A numeric vector or a numeric matrix with dB values.
-#' @param level Character. Specify in what scale your data is. \code{SPL} for Sound Pressute Level or \code{IL} for Intensity Level. (By default \code{SPL})
-#' @param na.rm Logical. Argument passed to \code{\link[base]{mean}}. Should NA be removed? (By default FALSE)
+#' @param x Numeric. A vector or a matrix with decibels values (dB).
+#' @param level Character. Specify the factor for conversion. Use \code{SPL} (Sound Pressure Level) for amplitude-base data, with factor 20, or \code{IL} (Intensity Level) for power-base data, with fator 10 (By default \code{SPL}).
+#' @param na.rm Logical. Argument passed to \code{\link[base]{mean}}. If \code{TRUE} removes NA (By default \code{FALSE}).
 #'
-#' @details This function converts your dB data to linear values (through \code{\link{dBtoLinear}} function), compute the Root Mean Square (rms), and converts the result back to dB (through \code{\link{LineartodB}} function).
-#' @details This function was adapted from \code{\link[seewave]{meandB}} and \code{\link[seewave]{rms}} functions from \code{\link[seewave]{seewave}} package. See their help for more details.
+#' @details This function converts the decibels data to linear values using the \code{\link{dBtoLinear}} function, computes the Root Mean Square (rms), and then converts the result back to decibels using the \code{\link{LineartodB}} function.
+#' @details This function s adapted from the \code{\link[seewave]{meandB}} and \code{\link[seewave]{rms}} functions from \code{\link[seewave]{seewave}} package. See their documentation for more details.
 #'
-#' @return A numeric value that represents the root mean square of x.
+#' @return A numeric value that representing the root mean square of x.
 #'
 #' @seealso \code{\link[seewave]{meandB}}, \code{\link[seewave]{rms}}
 #'
