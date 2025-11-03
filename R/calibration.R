05b7b3a096d72a9b9653e161b12f8af8cd0a330a
Cássio Rachid Simões <cassiorachid@gmail.com>
2025-11-03 17:23:49 -0300

    calibration function inicial notes

---
+++ R/calibration.R
@@ -0,0 +1,14 @@
+calibration <- function(
+    file = NULL,
+    channel = "left",
+    from = 0,
+    to = Inf,
+    weighting = "none",
+    ref = 20,
+    leqsignal = NA
+){
+    # usar leqbands em dBFS
+    # pegar Leq
+    # comparar com o valor de referência (leqcalib)
+    # calcular o valor de calibração
+}
